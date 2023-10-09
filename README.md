edbab4e466685e652bfca92d2e2502496d5523f5
Andrew <andrewshmorhun@gmail.com>
2023-10-09 11:20:02 +0300

    edit home page

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## To Do:
 
 - [ ] i18n
-- [ ] Unit tests
 - [ ] JWT
-- [ ] Web storage
 - [ ] Clean Up
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource theme-ui */
 "use client";
 import { FlexBox } from "components/lib/Boxes";
+import Link from "next/link";
 import { Box, Button, Grid } from "theme-ui";
 
 export function HomePage() {
@@ -9,24 +10,31 @@ export function HomePage() {
       flex="column"
       sx={{
         minHeight: "100%",
+        gap: 4,
       }}
     >
       <h1 sx={{ variant: "styles.headers.main" }}>Welcome</h1>
-      <Button sx={{ variant: "styles.buttons.primary" }}>get started</Button>
-      <Grid gap={3} columns={[2, "1fr 2fr"]} sx={{ pt: 3 }}>
-        <Box bg="primary.main" p={1}>
-          Task 1
+      <Grid gap={3} columns={[2, "1fr 4fr"]} sx={{ pt: 3, fontSize: 3, mx: 7 }}>
+        <Box bg="primary.main" p={1} sx={{ variant: "styles.box.flex.center" }}>
+          Add products
         </Box>
         <Box bg="muted" p={1}>
-          Description 1
+          To add products press the "Add" button
         </Box>
-        <Box bg="primary.main" p={1}>
-          Task 2
+        <Box bg="primary.main" p={1} sx={{ variant: "styles.box.flex.center" }}>
+          Delete products
         </Box>
         <Box bg="muted" p={1}>
-          Description 2
+          Delete method implemented with experimental "acton"
+          functionality(without page rerender). Implemented in groups and income
+          pages
         </Box>
       </Grid>
+      <Link href={"/products"}>
+        <Button sx={{ variant: "styles.buttons.primary", mt: 4 }}>
+          get started
+        </Button>
+      </Link>
     </FlexBox>
   );
 }
